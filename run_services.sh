cb7c2bd7be833a69408774371dabdf7ff6768d87
Charles Anthony <charlesanthony296@gmail.com>
2024-05-31 10:57:06 +0200

    llm changes

--- run_services.sh
+++ run_services.sh
@@ -13,13 +13,15 @@ EOF
 export FLASK_APP_DIR="$(pwd)"
 
 # Commands to run in each service's directory
-CMD1="cd '$FLASK_APP_DIR/AI_API-Wrapper/openai_backend' && nodemon --exec python3 llm_backend.py"
-CMD2="cd '$FLASK_APP_DIR/AI_Filter/filter' && nodemon --exec python3 twitterRoberta.py"
-CMD3="cd '$FLASK_APP_DIR/AI_Server/server' && nodemon --exec python3 server.py"
+CMD1="cd '$FLASK_APP_DIR/AI_API-Wrapper' && nodemon --exec python3 llm_backend.py"
+CMD2="cd '$FLASK_APP_DIR/AI_Filter' && nodemon --exec python3 twitterRoberta.py"
+CMD3="cd '$FLASK_APP_DIR/AI_Server' && nodemon --exec python3 server.py"
+CMD4="cd '$FLASK_APP_DIR/AI_Database' && nodemon --exec python3 database.py"
 
 # Open new Terminal tabs for each command
 open_new_tab "$CMD1"
 open_new_tab "$CMD2"
 open_new_tab "$CMD3"
+open_new_tab "$CMD4"
 
 echo "Services are starting in separate terminal tabs..."
